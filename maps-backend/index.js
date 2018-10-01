08dc2f5a61d4afbbb26096a200158364aacd5b6c
Lindroos Heidi <lindroh1@heidilaptop.org.aalto.fi>
2018-10-01 09:53:11 +0300

    modify "database"

--- maps-backend/index.js
+++ maps-backend/index.js
@@ -5,41 +5,8 @@ const bodyParser = require('body-parser')
 app.use(express.static('build'))
 app.use(bodyParser.json())
 
-let paths = [
-    {
-      "content": [
-        {
-          "name": "Path 1",
-          "key": 1538157896402,
-          "path": [
-            {
-              "position": {
-                "lat": 60.158532226201196,
-                "lng": 24.9289542327881
-              },
-              "key": 1538157892371
-            },
-            {
-              "position": {
-                "lat": 60.161906104440234,
-                "lng": 24.91848288879396
-              },
-              "key": 1538157893253
-            },
-            {
-              "position": {
-                "lat": 60.16677412839074,
-                "lng": 24.922688592529312
-              },
-              "key": 1538157894654
-            }
-          ]
-        }
-      ],
-      "id": 5
-    }
- ]
-
+// "database" to save jsons received from client side
+let paths = []
 
 
 app.get('/paths', (req, res) => {

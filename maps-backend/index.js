f1e4424e358fb60e42b679cb06b5bdd65a08d3bf
Lindroos Heidi <lindroh1@heidilaptop.org.aalto.fi>
2018-10-01 09:42:10 +0300

    modify "database"

--- maps-backend/index.js
+++ maps-backend/index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const cors = require('cors')
 
-app.use(cors())
 app.use(express.static('build'))
 app.use(bodyParser.json())
 
